ae04c401b4a6b76e4fe90a6190109d3559e203ef
statmlben <bendaimath@gmail.com>
2021-07-12 12:57:49 +0800

    update sim_CI.py

--- sim_CI.py
+++ sim_CI.py
@@ -23,7 +23,7 @@ for beta0 in [.05]:
 		theta0 = np.random.randn(p)
 		# theta0 = np.ones(p)
 		theta0 = theta0 / np.sqrt(np.sum(theta0**2))
-		Z, X, y, phi = sim(n, p, theta0, beta0, case='log', feat='cate')
+		Z, X, y, phi = sim(n, p, theta0, beta0, case='inverse', feat='cate')
 		if abs(X).max() > 1e+8:
 			i = i - 1
 			continue
