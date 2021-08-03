e0cfb34cb4b2c1de07d891d21b580142115b70dc
statmlben <bendaimath@gmail.com>
2021-08-03 11:15:25 +0800

    add number of slices

--- app.py
+++ app.py
@@ -13,10 +13,11 @@ np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 df = {'gene': [], 'p-value': [], 'beta': [], 'method': [], 'CI': []}
 gene_files = ['A1BGJuly20_2SIR', 'ABCA7July20_2SIR', 'ABHD8July20_2SIR', 'ACER1July20_2SIR', 'ACP5July20_2SIR', 
 			'ACPTJuly20_2SIR', 'ACSBG2July20_2SIR', 'ACTL9July20_2SIR', 'ACTN4July20_2SIR', 'ADAMTS10July20_2SIR']
+
 for file_tmp in gene_files:
-	# file_tmp = 'ACTN4July20_2SIR'
-	print('\n##### Causal inference of %s #####' %file_tmp)
-	# diff gene: ACP5July20_2SIR, 
+# for file_tmp in ['ABHD8July20_2SIR']:
+	gene_code = file_tmp.replace('July20_2SIR', '')
+	print('\n##### Causal inference of %s #####' %gene_code)
 	## load data
 	dir_name = '~/GenesToAnalyze/'+file_tmp
 	sum_stat = pd.read_csv(dir_name+"/sum_stat.csv", sep=' ', index_col=0)
@@ -27,11 +28,11 @@ for file_tmp in gene_files:
 	LD_Z1, cov_ZX1 = np.dot(snp.values.T, snp.values), np.dot(snp.values.T, gene_exp.values.flatten())
 	LD_Z2, cov_ZY2 = LD_Z1/n1*n2, sum_stat.values.flatten()*n2
 
-	Ks = range(int(p/2)-1)
+	Ks = range(int(p/2))
 	## 2SLS
 	reg_model = L0_IC(fit_intercept=False, alphas=10**np.arange(-5,3,.3),
 					Ks=Ks, max_iter=50000, refit=False, find_best=False)
-	LS = _2SLS(sparse_reg=reg_model)
+	LS = _2SLS(sparse_reg=None)
 	## Stage-1 fit theta
 	LS.fit_theta(LD_Z1, cov_ZX1)
 	## Stage-2 fit beta
@@ -48,7 +49,7 @@ for file_tmp in gene_files:
 	print('p-value based on 2SLS: %.5f' %LS.p_value)
 	print('CI based on 2SLS: %s' %(LS.CI))
 	## save the record
-	df['gene'].append(file_tmp)
+	df['gene'].append(gene_code)
 	df['method'].append('2SLS')
 	df['p-value'].append(LS.p_value)
 	df['beta'].append(LS.beta)
@@ -60,7 +61,7 @@ for file_tmp in gene_files:
 					Ks=Ks, max_iter=50000, refit=False, find_best=False)
 	PT_X1 = power_transform(gene_exp.values.reshape(-1,1), method='yeo-johnson').flatten()
 	PT_cor_ZX1 = np.dot(snp.values.T, PT_X1)
-	PT_LS = _2SLS(sparse_reg=reg_model)
+	PT_LS = _2SLS(sparse_reg=None)
 	## Stage-1 fit theta
 	PT_LS.fit_theta(LD_Z1, PT_cor_ZX1)
 	## Stage-2 fit beta
@@ -79,7 +80,7 @@ for file_tmp in gene_files:
 	print('p-value based on PT-2SLS: %.5f' %PT_LS.p_value)
 	print('CI based on 2SLS: %s' %(PT_LS.CI))
 	## save the record
-	df['gene'].append(file_tmp)
+	df['gene'].append(gene_code)
 	df['method'].append('PT-2SLS')
 	df['p-value'].append(PT_LS.p_value)
 	df['beta'].append(PT_LS.beta)
@@ -89,7 +90,7 @@ for file_tmp in gene_files:
 	## 2SIR
 	reg_model = L0_IC(fit_intercept=False, alphas=10**np.arange(-5,3,.3),
 					Ks=Ks, max_iter=50000, refit=False, find_best=False)
-	SIR = _2SIR(sparse_reg=reg_model)
+	SIR = _2SIR(sparse_reg=None, data_in_slice=0.1*n1)
 	## Stage-1 fit theta
 	SIR.fit_theta(Z1=snp.values, X1=gene_exp.values.flatten())
 	## Stage-2 fit beta
@@ -101,12 +102,11 @@ for file_tmp in gene_files:
 	print('p-value based on 2SIR: %.5f' %SIR.p_value)
 	print('CI based on 2SIR: %s' %(SIR.CI))
 	## save the record
-	df['gene'].append(file_tmp)
+	df['gene'].append(gene_code)
 	df['method'].append('2SIR')
 	df['p-value'].append(SIR.p_value)
 	df['beta'].append(SIR.beta)
 	df['CI'].append(SIR.CI)
-
 
 df = pd.DataFrame.from_dict(df)
 
@@ -120,9 +120,11 @@ sns.set_theme(style="whitegrid")
 # Draw a nested barplot by species and sex
 g = sns.catplot(
     data=df, kind="bar",
-    x="gene", y="p-value", hue="method", palette="dark", alpha=.6, height=6
+    x="gene", y="p-value", hue="method", palette="dark", alpha=.5, height=6, legend=False
 )
+plt.axhline(.05, ls='--', color='r', alpha=.8)
 g.despine(left=True)
 g.set_axis_labels("gene", "p-value")
-g.legend.set_title("")
+# g.legend.set_title("")
+plt.legend(loc='upper right')
 plt.show()
