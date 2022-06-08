e49d2bd78865fbd9a10323f164aa83e6726b6f6f
statmlben <bendaimath@gmail.com>
2022-06-08 21:08:35 +0800

    Jun8|add preprocessing low corr

--- nl_causal/base/preprocessing.py
+++ nl_causal/base/preprocessing.py
@@ -24,6 +24,36 @@ def variance_threshold_selector(data, threshold=0.05):
     selector.fit(data)
     return data[data.columns[selector.get_support(indices=True)]]
 
+def calculate_cor_(X, thresh=0.8, verbose=0):
+    """
+    Remove low-correlated features.
+
+    Parameters
+    ----------
+    X: {array-like, sparse matrix} of shape (n_samples, n_features)
+        Feature matrix
+
+    Returns
+    -------
+    X: return feature matrix by removing low-correlated features.
+    """
+    cols = X.columns
+    variables = np.array(range(X.shape[1]))
+    for ix in range(len(variables)):
+        if ix >= len(variables):
+            break
+        cor_tmp = np.zeros(len(variables))
+        cor_tmp[ix+1:] = X.iloc[:,variables].iloc[:,ix+1:].corrwith(X.iloc[:,variables].iloc[:,ix]).values
+        if max(cor_tmp) > thresh:
+            variables = variables[cor_tmp<thresh]
+            # print(len(variables))
+    if verbose:
+        print('Remaining variables:')
+        print(X.columns[variables])
+    cols_new = cols[variables]
+    return X.iloc[:, variables], cols_new
+    
+
 def calculate_vif_(X, thresh=2.5, verbose=0):
     """
     Remove multicollinearity features.
